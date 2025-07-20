c1a96498af6e67973ec05bacc2fc0b825cded8dc
MSI\USER <saikidd2002@yahoo.com.tw>
2025-07-21 03:23:46 +0800

    no message

--- Ryan.cpp
+++ Ryan.cpp
@@ -1,23 +1,51 @@
 
 #include <iostream>
+#include <cstdlib>  // for malloc/free
 
 namespace Ryan{
-    int a = 10;
+    static int a = 10;
     int b = 20;
 
+};
+
+static void test(){
+    std::cout<<Ryan::b<< std::endl;
+
 }
 
-int main(){
 
-    std::cout << Ryan::b<< std::endl;
-    return 0;
-}
 
-class RyanClass{
+class FatherClass{
     int a;
+    public:
     int sum(int a, int b){
         return a + b;
 
     }
+    void *ptr = malloc(5);
+};
+
+
+
+class SonClass: public FatherClass{
+    int a;
+    public:
+    int sum3(int a, int b){
+        return 5*a + 6*b;
+
+    }
+    
+};
+
+
+int main(){
+
+    std::cout << Ryan::b<< std::endl;
+    SonClass SS;
+    int res = SS.sum3(7,8);
+    std::cout << res << std::endl;
+    return res;
+    // return 0;
 
 }
+
