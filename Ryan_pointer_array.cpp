d3bf87130d2bca418e52542914f5164fca5da063
DESKTOP-OQJ6D6B\Ryan <saikidd2002@gmail.com>
2025-08-01 00:49:44 +0800

    no message

--- Ryan_pointer_array.cpp
+++ Ryan_pointer_array.cpp
@@ -1,7 +1,7 @@
  #include<iostream>
 
  int arr[] = {3,6,9};
-
+ int *arr2[] = {3,6,9};
  int main(){
     int len = sizeof(arr) / sizeof(arr[0]);  // 正確取得元素數量
     std::cout<<sizeof(arr)<<std::endl;
@@ -9,6 +9,8 @@
     for(int i=0;i<len;i++){
         std::cout<<arr[i]<<std::endl;
     }    
-
+    for(int i=0;i<len;i++){
+        std::cout<<arr2[i]<<std::endl;
+    }    
 
  }
